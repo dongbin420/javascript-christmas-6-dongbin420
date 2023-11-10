fa88b5fd3bc0aa95cce3e0750dcf6a57f425a645
dongbin420 <dongbin420@gmail.com>
2023-11-10 22:49:15 +0900

    feat: set up application initial structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import EventController from "./EventController.js";
+
 class App {
-  async run() {}
+  async run() {
+    const EVENT_CONTROLLER = new EventController();
+    await EVENT_CONTROLLER.start();
+  }
 }
 
 export default App;
---
+++ src/EventController.js
@@ -0,0 +1,5 @@
+class EventController {
+  async start() {}
+}
+
+export default EventController;
